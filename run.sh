c8eb4620137deca483bca85b094d0c01e6e0e3fe
Wei Tang <wei@that.world>
2020-11-07 23:41:26 +0100

    Remove custom worker count settings

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 python -c "import logging; logging.basicConfig(level=logging.DEBUG); from core import migrations; migrations.perform()"
 python -c "from core import indexes; indexes.create_indexes()"
 python startup.py
-(sleep 5 && curl -X POST -u :$POUSETACHES_AUTH_KEY $MICROBLOGPUB_POUSSETACHES_HOST/resume)&
-gunicorn -t 600 -w 5 -b 0.0.0.0:5005 --log-level debug app:app
+(sleep 5 && curl -X POST -u :$POUSETACHES_AUTH_KEY $MICROBLOGPUB_POUSSETACHES_HOST/resume) &
+gunicorn -b 127.0.0.1:5005 --log-level debug app:app
