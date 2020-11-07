6894f1b6a45e5b88314fbf5311b2bc97a0832b70
Wei Tang <wei@that.world>
2020-11-07 23:43:48 +0100

    Switch to raw flask for now for docker logs

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ python -c "import logging; logging.basicConfig(level=logging.DEBUG); from core i
 python -c "from core import indexes; indexes.create_indexes()"
 python startup.py
 (sleep 5 && curl -X POST -u :$POUSETACHES_AUTH_KEY $MICROBLOGPUB_POUSSETACHES_HOST/resume) &
-gunicorn -b 127.0.0.1:5005 --log-level debug app:app
+FLASK_APP=app.py flask run -p 5005 --with-threads
+
+# gunicorn -b 127.0.0.1:5005 --log-level debug app:app
